814bfc46830482e63e530e351e2f385cc8f1c373
Vuthaorn <158430959+Vuthaorn@users.noreply.github.com>
2024-05-27 16:44:09 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,365 @@
+const Product = [
+  // Coffee
+  {
+    id: "1",
+    name: "Coffee ice Latter",
+    img: "./image/cafe1 (1).png",
+    price: "3.99",
+    category: "Coffee",
+    description: "Coffee ice Latter",
+  },
+  {
+    id: "2",
+    name: "Vanilla",
+    img: "./image/cafe1 (2).png",
+    price: 3.99,
+    category: "Milk",
+    description: "Milk",
+  },
+  {
+    id: "3",
+    name: "Frappuccinos",
+    img: "./image/cafe1 (3).png",
+    price: "4.99",
+    category: "Cream",
+    description: "Frappuccinos",
+  },
+  {
+    id: "4",
+    name: "Burger",
+    img: "./image/cafe1 (4).png",
+    price: 3.99,
+    category: "Green tea",
+    description: "Ice Metcha Green tea latte",
+  },
+  {
+    id: "5",
+    name: "Mocha",
+    img: "./image/cafe1 (5).png",
+    price: 3.99,
+    category: "Coffee",
+    description: "Chocalate ice cream",
+  },
+  {
+    id: "6",
+    name: "Gingerbread Loaf",
+    img: "./image/cafe1 (6).png",
+    price: 3.99,
+    category: "Bread",
+    description: "Bread Cookies",
+  },
+  {
+    id: "7",
+    name: "Coffee ice Latter",
+    img: "https://i.pinimg.com/236x/e9/42/fb/e942fb292f4397aac14f2b2d34b56338.jpg",
+    price: "3.99",
+    category: "Coffee",
+    description: "Coffee ice Latter",
+  },
+  {
+    id: "8",
+    name: "Frappuccinos",
+    img: "https://i.pinimg.com/564x/8f/d0/02/8fd00239067397ef8559b8792e549e7f.jpg",
+    price: "4.99",
+    category: "Cream",
+    description: "Frappuccinos",
+  },
+  {
+    id: "9",
+    name: "Mocha",
+    img: "https://i.pinimg.com/564x/74/4c/47/744c476139522c89625c46a932d1daaf.jpg",
+    price: 3.99,
+    category: "Coffee",
+    description: "Chocalate ice cream",
+  },
+  {
+    id: "10",
+    name: "Ice Metcha Green tea latte",
+    img: "https://i.pinimg.com/564x/0c/e0/7f/0ce07f3642177b347d8d8b7cd855856e.jpg",
+    price: 3.99,
+    category: "Green tea",
+    description: "Ice Metcha Green tea latte",
+  },
+  {
+    id: "11",
+    name: "Ice Metcha Green tea latte",
+    img: "https://i.pinimg.com/236x/e9/42/fb/e942fb292f4397aac14f2b2d34b56338.jpg",
+    price: 3.99,
+    category: "Green tea",
+    description: "Ice Metcha Green tea latte",
+  },
+  {
+    id: "12",
+    name: "Coffee ice Latter",
+    img: "https://i.pinimg.com/236x/e9/42/fb/e942fb292f4397aac14f2b2d34b56338.jpg",
+    price: "3.99",
+    category: "Coffee",
+    description: "Coffee ice Latter",
+  },
+  {
+    id: "13",
+    name: "Frappuccinos",
+    img: "https://i.pinimg.com/564x/8f/d0/02/8fd00239067397ef8559b8792e549e7f.jpg",
+    price: "4.99",
+    category: "Cream",
+    description: "Frappuccinos",
+  },
+  {
+    id: "14",
+    name: "Mocha",
+    img: "https://i.pinimg.com/564x/74/4c/47/744c476139522c89625c46a932d1daaf.jpg",
+    price: 3.99,
+    category: "Coffee",
+    description: "Chocalate ice cream",
+  },
+  {
+    id: "15",
+    name: "Ice Metcha Green tea latte",
+    img: "https://i.pinimg.com/564x/0c/e0/7f/0ce07f3642177b347d8d8b7cd855856e.jpg",
+    price: 3.99,
+    category: "Green tea",
+    description: "Ice Metcha Green tea latte",
+  },
+  {
+    id: "16",
+    name: "Coffee ice Latter",
+    img: "https://i.pinimg.com/236x/e9/42/fb/e942fb292f4397aac14f2b2d34b56338.jpg",
+    price: "3.99",
+    category: "Coffee",
+    description: "Coffee ice Latter",
+  },
+  {
+    id: "17",
+    name: "Frappuccinos",
+    img: "https://i.pinimg.com/564x/8f/d0/02/8fd00239067397ef8559b8792e549e7f.jpg",
+    price: "4.99",
+    category: "Cream",
+    description: "Frappuccinos",
+  },
+  {
+    id: "18",
+    name: "Mocha",
+    img: "https://i.pinimg.com/564x/74/4c/47/744c476139522c89625c46a932d1daaf.jpg",
+    price: 3.99,
+    category: "Coffee",
+    description: "Chocalate ice cream",
+  },
+
+  //Bread
+  {
+    id: "19",
+    name: "Gingerbread Loaf",
+    img: "https://i.pinimg.com/236x/e9/42/fb/e942fb292f4397aac14f2b2d34b56338.jpg",
+    price: 3.99,
+    category: "Bread",
+    description: "Bread Cookies",
+  },
+  {
+    id: "20",
+    name: "Gingerbread Loaf",
+    img: "https://i.pinimg.com/236x/e9/42/fb/e942fb292f4397aac14f2b2d34b56338.jpg",
+    price: 3.99,
+    category: "Bread",
+    description: "Bread Cookies",
+  },
+  {
+    id: "21",
+    name: "Gingerbread Loaf",
+    img: "https://i.pinimg.com/236x/e9/42/fb/e942fb292f4397aac14f2b2d34b56338.jpg",
+    price: 3.99,
+    category: "Bread",
+    description: "Bread Cookies",
+  },
+  {
+    id: "22",
+    name: "Gingerbread Loaf",
+    img: "https://i.pinimg.com/236x/e9/42/fb/e942fb292f4397aac14f2b2d34b56338.jpg",
+    price: 3.99,
+    category: "Bread",
+    description: "Bread Cookies",
+  },
+  {
+    id: "23",
+    name: "Gingerbread Loaf",
+    img: "https://i.pinimg.com/236x/e9/42/fb/e942fb292f4397aac14f2b2d34b56338.jpg",
+    price: 3.99,
+    category: "Bread",
+    description: "Bread Cookies",
+  },
+  {
+    id: "24",
+    name: "Gingerbread Loaf",
+    img: "https://i.pinimg.com/236x/e9/42/fb/e942fb292f4397aac14f2b2d34b56338.jpg",
+    price: 3.99,
+    category: "Bread",
+    description: "Bread Cookies",
+  },
+  {
+    id: "25",
+    name: "Gingerbread Loaf",
+    img: "https://i.pinimg.com/236x/e9/42/fb/e942fb292f4397aac14f2b2d34b56338.jpg",
+    price: 3.99,
+    category: "Bread",
+    description: "Bread Cookies",
+  },
+];
+// Output the List Product
+// const allProducts = document.querySelector(".product-list");
+
+// function renderProducts() {
+//   let html = "";
+//   // Use ForEach to loop through all products array
+//   Product.forEach((Product) => {
+//     html =
+//       html +
+//       `
+//     <div class="col-lg-3 col-12 box">
+//         <div class="card mb-3">
+//             <img class "card-img-top rounded-3 img" src="${Product.img}" alt="Card image cap ">
+//             <div class = "card-body mb-3">
+//             <h5 class = "card-title food-title">${Product.name}</h5>
+//             <p class = "card-text">${Product.description}</p>
+//             <p class = "card-text price">Price : $${Product.price}</p>
+//             <a href="#" class="btn btn-outline-primary buy">Add  Card</a>
+//             </div>
+//         </div>
+//     </div>
+//         `;
+//   });
+//   return html;
+// }
+// allProducts.innerHTML = renderProducts();
+
+// Event listener for adding a product to the cart
+function handleAddToCart(event) {
+  if (event.target.classList.contains('buy')) {
+    const productName = event.target.closest('.box').querySelector('.food-title').textContent;
+    const product = Product.find(p => p.name === productName);
+    addToCart(Product);
+  }
+}
+
+// Function to add a product to the cart
+function addToCart(Product) {
+  // Implement logic to add the product to the cart
+  console.log('Product added to cart:', Product);
+}
+
+// scroll we change navbar background color
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 0) {
+    navbar.classList.add("fixed-top");
+  } else {
+    navbar.classList.remove("fixed-top");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const moonIcon = document.querySelector(".moon");
+  const navbar = document.querySelector(".navbar");
+  const body = document.body;
+
+  moonIcon.addEventListener("click", function () {
+    body.classList.toggle("dark-mode");
+    navbar.classList.toggle("dark-mode");
+    moonIcon.classList.toggle("bi-sun");
+  });
+});
+
+// Search Products
+const SearchInput = document.querySelector("#search-item");
+const allProduct = document.querySelector(".product-list");
+
+function Output(Product) {
+  let html = "";
+  Product.forEach((Products) => {
+    html += `
+    <div class="col-lg-4 col-md-6 col-12 box">
+    <div class="card mb-3">
+        <img class "card-img-top rounded-3 food-img" src="${Products.img}" alt="Card image cap ">
+        <div class = "card-body mb-3">
+        <h5 class = "card-title food-title'">${Products.name}</h5>
+        <p class = "card-text">${Products.description}</p>
+        <p class = "card-text price">Price : $${Products.price}</p>
+        <button class="btn btn-primary add">Add to Card</button>
+        </div>
+    </div>
+</div>
+    `;
+  });
+  return html;
+}
+function updateProductlist(Product) {
+  allProduct.innerHTML = Output(Product);
+}
+
+// Intialize Render function
+updateProductlist(Product);
+
+// Event Listeners for Search Input
+SearchInput.addEventListener("input", function () {
+  const SearchTerm = SearchInput.value.toLowerCase();
+  const FilterProduct = Product.filter((Products) =>
+    Products.name.toLowerCase().includes(SearchTerm)
+  );
+  updateProductlist(FilterProduct);
+});
+
+// Change Darkmode
+// Saerch Product
+const Search = document.querySelector("#search-item");
+Search.addEventListener("keyup", function (e) {
+  if (e.key === "Enter") {
+    let value = e.value.toLowerCase();
+    // Input  coffee
+
+    let check = Product.filter((p) => p.name.toLowerCase().includes(value));
+    updateProductlist(check);
+  }
+});
+const AllProduct = document.querySelector("#all");
+AllProduct.addEventListener("click", function () {
+  updateProductlist(Product);
+});
+
+// All Bread
+const AllBread = document.querySelector("#btn-1");
+AllBread.addEventListener("click", function () {
+  updateProductlist(Product.filter((b) => b.category === "Bread", "bread"));
+});
+
+// All Cafe
+const AllCoffee = document.querySelector("#btn-4");
+AllCoffee.addEventListener("click", function () {
+  updateProductlist(Product.filter((c) => c.category === "Coffee"));
+});
+
+// All Milk
+const AllMilk = document.querySelector("#btn-5");
+AllMilk.addEventListener("click", function () {
+  updateProductlist(Product.filter((m) => m.category === "Milk"));
+});
+
+// Animetion Logo Starbuck
+// function loader() {
+//   document.querySelector(".loader-container").classList.add("fade-out");
+// }
+// function FadOut() {
+//   setInterval(loader, 3000);
+// }
+// window.onload = FadOut();
+
+
+// Display Product List in Table
+const btnCart = document.querySelector('.buy');
+const cart = document.querySelector('.cart');
+const btnClose = document.querySelector('#cart-close');
+
+btnCart.addEventListener('click',()=> {
+  cart.classList.add('cart-active');
+});
+
+btnClose.addEventListener('click',()=> {
+  cart.classList.remove('cart-active');
+});
